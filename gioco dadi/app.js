456e750b04756e2781d4220432ec99e643605826
Simone823 <simone.daglio823@outlook.it>
2022-02-09 15:08:51 +0100

    modifica player 1

--- gioco dadi/app.js
+++ gioco dadi/app.js
@@ -6,25 +6,21 @@ console.log(btnPlayer1);
 // Al clic del bottone compare la sezione con il numero generato
 btnPlayer1.addEventListener("click", function(){
 
-    
-
-
 
     // Creo una variabile di numeri random da 1 a 6
-    const numbers = Math.floor(Math.random()* 6 + 1);
-    console.log("numero generato", numbers);
+    const numbersRandomPlayer1 = Math.floor(Math.random()* 6 + 1);
+    console.log("numero generato", numbersRandomPlayer1);
 
     // Seleziono il paragrafo numbers player 1 dal DOM
     const numbersPlayer1 = document.querySelector(".numbers");
     console.log(numbersPlayer1);
 
     // Stampo il numero generato nel dom
-    numbersPlayer1.innerHTML = numbers;
+    numbersPlayer1.innerHTML = numbersRandomPlayer1;
 
-
-
-// Recupero dal dom la sezione in display none
-const generatorResults = document.querySelector(".generated_number");
+    // Recupero dal dom la sezione in display none
+    const generatorResults = document.querySelector(".generated_number");
+   
     // Aggiungo la classe active a generatorResults
     generatorResults.classList.add("active");
     console.log(generatorResults);
