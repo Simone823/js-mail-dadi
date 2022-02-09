4840b45219f7e38b0705485148689e1ee466e994
Simone823 <simone.daglio823@outlook.it>
2022-02-09 15:40:52 +0100

    number random e pulsante player 2

--- gioco dadi/app.js
+++ gioco dadi/app.js
@@ -12,17 +12,43 @@ btnPlayer1.addEventListener("click", function(){
     console.log("numero generato", numbersRandomPlayer1);
 
     // Seleziono il paragrafo numbers player 1 dal DOM
-    const numbersPlayer1 = document.querySelector(".numbers");
+    const numbersPlayer1 = document.querySelector(".numbers_player1");
     console.log(numbersPlayer1);
 
     // Stampo il numero generato nel dom
     numbersPlayer1.innerHTML = numbersRandomPlayer1;
 
     // Recupero dal dom la sezione in display none
-    const generatorResults = document.querySelector(".generated_number");
+    const generatorResults = document.querySelector(".generated_number_player1");
    
     // Aggiungo la classe active a generatorResults
     generatorResults.classList.add("active");
     console.log(generatorResults);
 
+})
+
+// Recupero dal dom il pulsante del player 2 
+const btnPlayer2 = document.querySelector(".btn_player2");
+console.log(btnPlayer2);
+
+
+// Al clic del pulsante player 2 compare la sezione con il numero generato
+btnPlayer2.addEventListener("click", function() {
+        // Creo una variabile di numeri random da 1 a 6
+        const numbersRandomPlayer2 = Math.floor(Math.random()* 6 + 1);
+        console.log("numero generato", numbersRandomPlayer2);
+    
+        // Seleziono il paragrafo numbers player 2 dal DOM
+        const numbersPlayer2 = document.querySelector(".numbers_player2");
+        console.log(numbersPlayer2);
+    
+        // Stampo il numero generato nel dom
+        numbersPlayer2.innerHTML = numbersRandomPlayer2;
+    
+        // Recupero dal dom la sezione in display none
+        const generatorResults = document.querySelector(".generated_number_player2");
+       
+        // Aggiungo la classe active a generatorResults
+        generatorResults.classList.add("active");
+        console.log(generatorResults);
 })
