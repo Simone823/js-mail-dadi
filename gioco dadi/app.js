bacc7b9ece968ad002d2f758b30e0d018e5c5831
Simone823 <simone.daglio823@outlook.it>
2022-02-09 16:15:55 +0100

    risultato vincita o perdita

--- gioco dadi/app.js
+++ gioco dadi/app.js
@@ -2,14 +2,13 @@
 const btnPlayer1 = document.querySelector(".btn_player1");
 console.log(btnPlayer1);
 
+// Creo una variabile di numeri random da 1 a 6 player 1 
+const numbersRandomPlayer1 = Math.floor(Math.random()* 6 + 1);
+console.log("numero generato", numbersRandomPlayer1);
+
 
 // Al clic del bottone compare la sezione con il numero generato
 btnPlayer1.addEventListener("click", function(){
-
-
-    // Creo una variabile di numeri random da 1 a 6
-    const numbersRandomPlayer1 = Math.floor(Math.random()* 6 + 1);
-    console.log("numero generato", numbersRandomPlayer1);
 
     // Seleziono il paragrafo numbers player 1 dal DOM
     const numbersPlayer1 = document.querySelector(".numbers_player1");
@@ -24,20 +23,20 @@ btnPlayer1.addEventListener("click", function(){
     // Aggiungo la classe active a generatorResults
     generatorResults.classList.add("active");
     console.log(generatorResults);
-
 })
+
 
 // Recupero dal dom il pulsante del player 2 
 const btnPlayer2 = document.querySelector(".btn_player2");
 console.log(btnPlayer2);
 
+ // Creo una variabile di numeri random da 1 a 6 player2
+ const numbersRandomPlayer2 = Math.floor(Math.random()* 6 + 1);
+ console.log("numero generato", numbersRandomPlayer2);
 
 // Al clic del pulsante player 2 compare la sezione con il numero generato
 btnPlayer2.addEventListener("click", function() {
-        // Creo una variabile di numeri random da 1 a 6
-        const numbersRandomPlayer2 = Math.floor(Math.random()* 6 + 1);
-        console.log("numero generato", numbersRandomPlayer2);
-    
+       
         // Seleziono il paragrafo numbers player 2 dal DOM
         const numbersPlayer2 = document.querySelector(".numbers_player2");
         console.log(numbersPlayer2);
@@ -52,3 +51,28 @@ btnPlayer2.addEventListener("click", function() {
         generatorResults.classList.add("active");
         console.log(generatorResults);
 })
+
+
+// Recupero dal dom il paragrafo dove stampare la vincita o la perdita
+const result = document.querySelector(".win_or_lose");
+console.log(result);
+
+// Recupero il pulsante risultati dal dom
+const btnResult = document.querySelector(".btn_result");
+console.log(btnResult);
+
+// Al clic del pulsante risultati apro la sezione con il risultato
+btnResult.addEventListener("click", function() {
+    result.classList.add("active");
+    if (numbersRandomPlayer1 === numbersRandomPlayer2){
+        console.log("Pareggio");
+        result.innerHTML = "Pareggio";
+       } else if (numbersRandomPlayer1 > numbersRandomPlayer2){
+           console.log("Vince il player 1");
+           result.innerHTML = "Vince il player 1";
+       } else {
+           console.log("Vince il player 2");
+           result.innerHTML = "Vince il player 2";
+    }
+})
+
