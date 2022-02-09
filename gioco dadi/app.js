d6259ff779a2daad14c9d9a9c406cb113335fabe
Simone823 <simone.daglio823@outlook.it>
2022-02-09 14:47:01 +0100

    genero un numero casuale al click btn player1

---
+++ gioco dadi/app.js
@@ -0,0 +1,32 @@
+// Recupero dal dom il pulsante del player 1
+const btnPlayer1 = document.querySelector(".btn_player1");
+console.log(btnPlayer1);
+
+
+// Al clic del bottone compare la sezione con il numero generato
+btnPlayer1.addEventListener("click", function(){
+
+    
+
+
+
+    // Creo una variabile di numeri random da 1 a 6
+    const numbers = Math.floor(Math.random()* 6 + 1);
+    console.log("numero generato", numbers);
+
+    // Seleziono il paragrafo numbers player 1 dal DOM
+    const numbersPlayer1 = document.querySelector(".numbers");
+    console.log(numbersPlayer1);
+
+    // Stampo il numero generato nel dom
+    numbersPlayer1.innerHTML = numbers;
+
+
+
+// Recupero dal dom la sezione in display none
+const generatorResults = document.querySelector(".generated_number");
+    // Aggiungo la classe active a generatorResults
+    generatorResults.classList.add("active");
+    console.log(generatorResults);
+
+})
