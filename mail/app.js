754282b58dccf75155b64001b21ec50e10fcbd9c
Simone823 <simone.daglio823@outlook.it>
2022-02-10 09:27:23 +0100

    modifica ciclo for e if usermail != da verfiedEm

--- mail/app.js
+++ mail/app.js
@@ -8,14 +8,27 @@ const verifiedEmails = [
     "ciao123@outlook.it",
     "effetti@gmail.com"
 ];
+console.log(verifiedEmails);
 
 // Ciclo for indice verifiedEmails
 for (let i = 0; i < verifiedEmails.length; i++){
     console.log(verifiedEmails[i]);
 
-    if (userMail == verifiedEmails[i]){
-        console.log("Email Verificata");
-        alert("Email Verificata");
+    var element = verifiedEmails[i];
+    console.log(element);
+
+
+    // Se la mail inserita dall'utente è inserita in lista verifiedEmail
+    if (userMail == element){
+        console.log("Email in lista");
+        alert("Email in lista");
     }
     
+}
+
+
+// Se la mail inserita dall'utente non è in lista verfiedEmails
+if(userMail !== element){
+     console.log("email non in lista");
+     alert("Email non in lista, non hai i permessi per accedere!");
 }
